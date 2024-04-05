898cb0ae117dcafafd45eb0046e4394fa6333b24
Saumyasharma06 <154654575+Saumyasharma06@users.noreply.github.com>
2024-04-05 09:11:12 +0530

    Add files via upload

---
+++ day 3/js_intro/script.js
@@ -0,0 +1,141 @@
+// function em(){
+    
+//         console.log(message);
+// }
+
+//  let message="hello";
+//  console.log(message);
+
+// var message="hello world";
+// console.log(message);
+// message="else";
+// console.log(message);
+
+// const message="hello";
+// console.log(message);
+
+// let age=10;
+// console.log(typeof(age));
+
+// let userAge = "abcd";
+//age="abcd";
+// console.log(typeof(age));
+
+// let arr=['a','b','c','d'];
+// arr[0]="abcd";
+// console.log(arr);
+
+// let str="12";
+// let age = 20;
+// let ans=age+str;
+// console.log(ans);
+
+// let str='12';
+// const p=parseInt(str);
+// let n=number(str);
+// console.log(p,n);
+
+// const str='hey';
+// const user='how are you';
+// const ans=`${str}${user}`;
+// console.log(ans);
+
+// function print(str){
+//     console.log(str);
+// }
+// const s='hello';
+// print(s)
+// function sum(a,b){
+//     if(a&&b){
+//         console.log(a+b);
+//     }
+//     else
+//     console.log('no value')
+// }
+// sum(20,8);
+// let a=1;
+// if(a){
+//     console.log("yes")
+// }
+// else{
+//     console.log("no")
+// }
+
+// function declaration
+// function print(){
+//     console.log('--');
+// }
+// //function assignment(named)
+// const a=function printf(){
+//     console.log('--');
+// }
+
+// // function assignment(anonymous)
+// const b=()=>{
+//     console.log('--');
+// }
+// print()
+// a()
+// b()
+
+// const obj= new Object();
+// obj[12]="twelve";
+// obj[1]="one"
+// obj['name']="saumya"
+// console.log(obj);
+
+const x=prompt("please enter the name");
+console.log(x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

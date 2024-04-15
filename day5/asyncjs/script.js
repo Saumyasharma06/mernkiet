cdc11c4224659d67a0936dc6ab9c4e2cab5602ca
Saumyasharma06 <154654575+Saumyasharma06@users.noreply.github.com>
2024-04-15 22:24:46 +0530

    Add files via upload

---
+++ day5/asyncjs/script.js
@@ -0,0 +1,29 @@
+// console.log('GEC Start')
+function printPretty(elem){
+    // console.log(':'.elem)
+    console.log("PrettyStart");
+    let ans=2+3;
+    console.log(ans);
+    console.log("prettyEnd")
+}
+// setTimeout(printPretty, 10000);
+// console.log('GEC End')
+// const btn=document.getElementById('btn');
+// btn.addEventListener("click",printPretty)
+// console.log('start');
+// const req=fetch()
+// console.log(req);
+// req.then(()=>console.log("fetched"));
+
+// console.log('end');
+// console.log('start');
+// const req =fetch();
+// const res=req.then(res => res.json())
+// res.then((data)=>console.log(data));
+// console.log('end');
+// console.log('start')
+// fetch('https://dummyjson.com/products/1')
+// .then(res => res.json())
+// .then(json => console.log(json))
+// console.log('end')
+            
---
+++ day5/script.js
@@ -0,0 +1,26 @@
+// let arr=[2,20,6,54];
+// function printArray(arr){
+//     for(let i=0;i<arr.length;i++){
+//         console.log(arr[i])
+//     }
+// }
+// printArray(arr);
+
+// function printPretty(elem){
+//     console.log(':',elem);
+// }
+// function printArray(arr){
+//     for(let i=0;i<arr.length;i++){
+//         console.log(arr[i])
+//     }
+// arr.forEach((a))=>{
+//     console.log((a,b,c))=>{
+//         console.log(':',a,b,c)
+//     }
+// }
+// window.setTimeout(fn, time);
+function printPretty(elem){
+    // console.log(':'.elem)
+    console.log("hello")
+}
+setTimeout(printPretty, 10000);
